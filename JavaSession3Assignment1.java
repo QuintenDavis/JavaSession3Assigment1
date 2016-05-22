009cf8d8cf6e2fa65c72a2b5f2803fe0b8410fc3
QuintenDavis <Quinten.R.Davis@gmail.com>
2016-05-22 17:08:12 -0400

    Add files via upload

---
+++ JavaSession3Assignment1.java
@@ -0,0 +1,22 @@
+
+public class JavaSession3Assignment1 {
+
+	public static void main(String[] args) {
+	double cube = 125.0d;
+	double sqaure = 100.0d;
+	
+	//
+	// Get the cube root of double value
+	//
+	double cbrt = Math.cbrt(cube);
+	System.out.println("Cube root of " + cube + " is " +  cbrt);
+	
+	//
+	// Get the square root of double value
+	//
+	double sqrt = Math.sqrt(sqaure);
+	System.out.println("Square root of " + sqaure + " is " + sqrt);
+
+	}
+
+}
